b85edc5bc4025822f55a7c67dd55143c772d307e
Brandon Robinson <brandonscottrobinson@gmail.com>
2020-08-05 14:30:53 -0400

    makecardfunction

--- script.js
+++ script.js
@@ -4,7 +4,35 @@ let gameBoard = document.querySelector(".gameboard");
 
 let cardContainer = document.querySelector(".cardcontainer");
 
-let card = document.createElement("div");
-card.classList.add("card");
+const makeCard = (name, shape) => {
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.setAttribute("name", name);
+  card.setAttribute("data-shape", shape);
+  console.log(card);
+  cardContainer.append(card);
+  return card;
+};
 
-cardContainer.append(card);
+let cardList = [
+  {
+    name: "card1",
+    shape: "triangle",
+  },
+  {
+    name: "card2",
+    shape: "triangle",
+  },
+  {
+    name: "card3",
+    shape: "square",
+  },
+  {
+    name: "card4",
+    shape: "square",
+  },
+];
+
+cardList.forEach((card) => {
+  makeCard(card.name, card.shape);
+});
